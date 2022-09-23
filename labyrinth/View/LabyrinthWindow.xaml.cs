4afa155253644d4249bffed11b719235577dcdd5
golosalex <golosalex1>
2022-09-23 05:19:54 +0300

    InitProj Common MVVM filestructure

--- labyrinth/View/LabyrinthWindow.xaml.cs
+++ labyrinth/View/LabyrinthWindow.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace labyrinth
+namespace MazeProj.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for LabirintWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class LabyrinthWindow : Window
     {
-        public MainWindow()
+        public LabyrinthWindow()
         {
             InitializeComponent();
         }
